877c95a9d2b2b3b0ffb94f836dc01ba3287cedce
Md-Abdur-Rahman-07 <115738274+Md-Abdur-Rahman-07@users.noreply.github.com>
2023-06-07 21:47:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("btn-1").addEventListener("click",function(){
+    document.getElementById("btn-1").addEventListener("click",function(){
     const addAmount1 = document.getElementById("food").value;
     const addAmount2 = document.getElementById("rent").value;
     const addAmount3 = document.getElementById("clothes").value;
@@ -15,13 +15,6 @@ document.getElementById("btn-1").addEventListener("click",function(){
         document.getElementById("bal").innerHTML = addAmount5;
     }
     else{alert( "can't calculate..." )}
-    
-
-    
-
-
-
-
 });
  
 document.getElementById("btn-2").addEventListener("click",function(){
@@ -32,14 +25,28 @@ document.getElementById("btn-2").addEventListener("click",function(){
     const addAmount4 = document.getElementById("income").value;
 
     const addAmount = parseFloat(addAmount1) + parseFloat(addAmount2) + parseFloat(addAmount3);
-    const addAmount5 = parseFloat(addAmount4) - parseFloat(addAmount);
+    // const addAmount5 = parseFloat(addAmount4) - parseFloat(addAmount);
+
+    if(parseFloat(addAmount4) > parseFloat(addAmount)){
+        const addAmount5 = parseFloat(addAmount4) - parseFloat(addAmount);
+        document.getElementById("tex").innerHTML = addAmount;
+        document.getElementById("bal").innerHTML = addAmount5;
+
+        const addAmount6 = document.getElementById("save").value;
+        const addAmount7 = parseFloat(addAmount5) * parseFloat(addAmount6)/100;
+
+        document.getElementById("save1").innerHTML = addAmount7;
+
+        document.getElementById("reb").innerHTML = parseFloat(addAmount5) - parseFloat(addAmount7);
+    }
+    else{alert( "can't calculate..." )}
 
     // const addAmount5 = document.getElementById("bal").value;
-    const addAmount6 = document.getElementById("save").value;
-    const addAmount7 = parseFloat(addAmount5) * parseFloat(addAmount6)/100;
+    // const addAmount6 = document.getElementById("save").value;
+    // const addAmount7 = parseFloat(addAmount5) * parseFloat(addAmount6)/100;
 
-    document.getElementById("save1").innerHTML = addAmount7;
+    // document.getElementById("save1").innerHTML = addAmount7;
 
-    document.getElementById("reb").innerHTML = parseFloat(addAmount5) - parseFloat(addAmount7);
+    // document.getElementById("reb").innerHTML = parseFloat(addAmount5) - parseFloat(addAmount7);
 
 });
